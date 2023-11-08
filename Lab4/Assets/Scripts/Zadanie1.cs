9d0d754a53c6ecd9a9b5fb9ec847e9f555fce448
AZalecki <Artur2802@tlen.pl>
2023-11-08 14:14:50 +0100

    add lab4

--- Lab4/Assets/Scripts/Zadanie1.cs
+++ Lab4/Assets/Scripts/Zadanie1.cs
@@ -12,13 +12,17 @@ public class Zadanie1 : MonoBehaviour
     public int objectsCount = 10;
     // obiekt do generowania
     public GameObject block;
+    Collider m_Collider;
+    public Material[] materials;
 
     void Start()
     {
-        int x = (int) transform.position[0];
-        int z = (int) transform.position[2];
-        List<int> pozycje_x = new List<int>(Enumerable.Range(x-5, x+5).OrderBy(x => Guid.NewGuid()).Take(objectsCount));
-        List<int> pozycje_z = new List<int>(Enumerable.Range(z-5, z+5).OrderBy(x => Guid.NewGuid()).Take(objectsCount));
+        int min_x = (int) m_Collider.bounds.min[0];
+        int max_x = (int) m_Collider.bounds.max[0];
+        int min_z = (int)m_Collider.bounds.min[2];
+        int max_z = (int)m_Collider.bounds.max[2];
+        List<int> pozycje_x = new List<int>(Enumerable.Range(min_x, max_x).OrderBy(x => Guid.NewGuid()).Take(objectsCount));
+        List<int> pozycje_z = new List<int>(Enumerable.Range(min_z, max_z).OrderBy(x => Guid.NewGuid()).Take(objectsCount));
         
         for(int i=0; i<objectsCount; i++)
         {
@@ -41,7 +45,10 @@ public class Zadanie1 : MonoBehaviour
         Debug.Log("wywołano coroutine");
         foreach(Vector3 pos in positions)
         {
+            Material material = materials[UnityEngine.Random.Range(0, 5)];
             Instantiate(this.block, this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
+            Renderer renderer = this.block.GetComponent<Renderer>();
+            renderer.material = material;
             yield return new WaitForSeconds(this.delay);
         }
         // zatrzymujemy coroutine
